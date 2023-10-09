daa1fd0879e766edd7f25c2f6543eca2c0c5d86c
jean519 <129189905+jean519@users.noreply.github.com>
2023-10-09 17:03:37 +0200

    commit

---
+++ src/components/Pokecard.jsx
@@ -0,0 +1,18 @@
+
+const Pokecard = () => {
+    return (
+        <div className="card-container" >
+        <figure className="card">
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/510.png"
+          alt="bulbasaur"
+          className="card-img"
+          width='250px'
+        />
+        <figcaption>PokeChat</figcaption>
+      </figure>
+        </div>
+    );
+};
+
+export default Pokecard;
