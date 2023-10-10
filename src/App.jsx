54d8b6bfa57aa20c98e3f9ae066488ae0b5402ea
jean519 <129189905+jean519@users.noreply.github.com>
2023-10-10 10:23:58 +0200

    ajout des props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
 import Pokecard from './components/Pokecard';
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 const App = () => {
+  const pokemon = pokemonList[0]; 
   return (
     <div>
-      <Pokecard />
+      <Pokecard pokemon={pokemon} /> 
     </div>
   );
 };
--- src/components/Pokecard.jsx
+++ src/components/Pokecard.jsx
@@ -1,34 +1,24 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-const Pokecard = () => {
-  const pokemon = pokemonList[0];
+const Pokecard = (props) => {
+  const { name, imgSrc } = props.pokemon;
 
   return (
     <div className="card-container">
       <figure className="card">
-        {pokemon.imgSrc ? (
+        {imgSrc ? (
           <img
-            src={pokemon.imgSrc}
-            alt={pokemon.name}
+            src={imgSrc}
+            alt={name}
             className="card-img"
             width="250px"
           />
         ) : (
           <p>???</p>
         )}
-        <figcaption>{pokemon.name}</figcaption>
+        <figcaption>{name}</figcaption>
       </figure>
     </div>
   );
 };
 
 export default Pokecard;
+
