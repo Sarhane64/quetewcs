cd7a25efed5803a05c0fc56f9fe3f4e5f9cbe585
jean519 <129189905+jean519@users.noreply.github.com>
2023-10-10 09:45:16 +0200

    ajout du dynamisme de mes carte

--- src/components/Pokecard.jsx
+++ src/components/Pokecard.jsx
@@ -1,18 +1,34 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 const Pokecard = () => {
-    return (
-        <div className="card-container" >
-        <figure className="card">
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/510.png"
-          alt="bulbasaur"
-          className="card-img"
-          width='250px'
-        />
-        <figcaption>PokeChat</figcaption>
+  const pokemon = pokemonList[0];
+
+  return (
+    <div className="card-container">
+      <figure className="card">
+        {pokemon.imgSrc ? (
+          <img
+            src={pokemon.imgSrc}
+            alt={pokemon.name}
+            className="card-img"
+            width="250px"
+          />
+        ) : (
+          <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
       </figure>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Pokecard;
