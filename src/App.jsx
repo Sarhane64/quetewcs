3b28f3c267a24978b7b3334bb150cb737d49231b
jean519 <129189905+jean519@users.noreply.github.com>
2023-10-11 10:34:45 +0200

    ajout des button

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+
+import { useState } from 'react';
 import Pokecard from './components/Pokecard';
 
 const pokemonList = [
@@ -7,15 +9,47 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
 const App = () => {
-  const pokemon = pokemonList[0  ]; 
+  const [indexPokemon, setIndexPokemon] = useState(0)
+
+  const next = () => {
+    if(indexPokemon < pokemonList.length - 1) {
+      setIndexPokemon((current => current + 1))
+    }
+  }
+
+  const precedent = () => {
+    if(indexPokemon > 0) {
+      setIndexPokemon((current => current - 1))
+    }
+  }
+
+  const pokemon = pokemonList[indexPokemon]
+
   return (
     <div>
-      <Pokecard pokemon={pokemon} /> 
+      <Pokecard pokemon={pokemon} />
+      <button onClick={precedent}>Precedent</button>
+      <button onClick={next}>Next</button> 
     </div>
   );
 };
